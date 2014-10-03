c55423fe841b0f1134e15358b05d7107baa2e9ab
Patrick <Patrick@Patrick-Alien>
2014-10-03 16:24:18 -0400

    Made your back button yw

--- src/screens/ServerEntryScreen.java
+++ src/screens/ServerEntryScreen.java
@@ -23,6 +23,7 @@ import ui.UIElement;
  */
 public class ServerEntryScreen extends Screen {
     protected Button addButton;
+    protected Button backButton;
     
     public ServerEntryScreen(Application app, Node rootNode, Node guiNode){
         super(app, rootNode, guiNode);
@@ -30,21 +31,37 @@ public class ServerEntryScreen extends Screen {
     }
 
     @Override
-    public void initialize(InputHandler inputHandler) {
+    public void initialize(final InputHandler inputHandler) {
         Util.log("[ServerEntryScreen] Initializing...", 1);
         float width = Sys.width;
         float height = Sys.height;
         
+        //Makes addButton
         addButton = new Button(gui, new Vector2f(width*0.4f, height*0.3f), width*0.3f, height*0.05f, 0){
             @Override
             public void onAction(Vector2f cursorLoc, String bind, boolean down, float tpf){
                 if(bind.equals(ClientBinding.LClick.toString()) && down){
-                    //
+                    //Does nothing yet should add server
                 }
             }
         };
         addButton.setColor(new ColorRGBA(0.5f, 0.5f, 0.5f, 1));
         addButton.setText("Add");
+        
+        //Makes back button
+        backButton = new Button(gui, new Vector2f(width*0.4f, height*0.2f), width*0.3f, height*0.05f, 0){
+            @Override
+            public void onAction(Vector2f cursorLoc, String bind, boolean down, float tpf){
+                //Click mouse goes back
+                if(bind.equals(ClientBinding.LClick.toString()) && down){
+                    inputHandler.switchScreens(new MultiplayerScreen(app, root.getParent(), gui.getParent()));
+                }
+            }
+        };
+        //Colors back button
+        backButton.setColor(new ColorRGBA(0.8f, 0f, 0f, 1));
+        backButton.setText("Back");
+        ui.add(backButton);
     }
 
     @Override
