827525267f320f7f20a653b5c25fef1af6f57742
Patrick <Patrick@Patrick-Alien>
2014-10-03 17:15:42 -0400

    Broke everything

--- src/items/Inventory.java
+++ src/items/Inventory.java
@@ -1,9 +1,35 @@
 package items;
+import java.util.ArrayList;
+import stats.Stat;
 
 /**
  *
  * @author SinisteRing
  */
 public class Inventory {
-    //
+    protected ArrayList<Item> items;
+    // Default constructor.
+    public Inventory(){
+        items = new ArrayList();
+    }
+    // Constructor with prior ArrayList of items
+    public Inventory(ArrayList<Item> items){
+        this.items = items;
+    }
+    
+    // Getters:
+    public ArrayList<Item> getList(){
+        return items;
+    }
+    
+    // Add an item priority-wise.
+    public void add(Item item){
+        for(Item i : items){
+            if(i.getClass().equals(item.getClass())){
+                //i.add(item.getCurrent());
+            }
+        }
+        items.add(item);
+    }
 }
+
